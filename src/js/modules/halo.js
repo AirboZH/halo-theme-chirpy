5c049c8c0316e34c999df9aa41999ca492797995
AirboZH <i@airbozh.cn>
2023-09-08 17:21:10 +0800

    feat: support comment plugin

---
+++ src/js/modules/halo.js
@@ -0,0 +1,16 @@
+export default {
+  flipMode: () => {
+    if (
+      document
+        .querySelector('#comment div div')
+        .shadowRoot.querySelector('.halo-comment-widget').classList != null
+    ) {
+      let commentDOMclass = document
+        .querySelector('#comment div div')
+        .shadowRoot.querySelector('.halo-comment-widget').classList;
+      if (commentDOMclass.contains('light'))
+        commentDOMclass.replace('light', 'dark');
+      else commentDOMclass.replace('dark', 'light');
+    }
+  }
+};
--- src/js/modules/layouts/sidebar.js
+++ src/js/modules/layouts/sidebar.js
@@ -1,7 +1,9 @@
 import { modeWatcher } from '../components/mode-watcher';
 import { sidebarExpand } from '../components/sidebar';
+import halo from '../halo';
 
 export function initSidebar() {
   modeWatcher();
   sidebarExpand();
+  window.halo = halo;
 }
