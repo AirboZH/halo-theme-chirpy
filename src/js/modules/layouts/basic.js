553478004c0560f5aabaabe5650d2bfe1bc280f4
AirboZH <i@airbozh.cn>
2023-09-13 15:51:36 +0800

    feat: 支持行内代码块样式

--- src/js/modules/layouts/basic.js
+++ src/js/modules/layouts/basic.js
@@ -1,7 +1,9 @@
 import { back2top } from '../components/back-to-top';
 import { loadTooptip } from '../components/tooltip-loader';
+import { codeClass } from '../../patch';
 
 export function basic() {
   back2top();
   loadTooptip();
+  codeClass();
 }
