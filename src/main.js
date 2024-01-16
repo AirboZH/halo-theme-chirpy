8d862f36cc33e1de1cee3d8dc12cadd0bdce53c7
Waite Wang <waite@waite.wang>
2024-01-16 16:59:58 +0800

    ✨ feat: 适配表格

--- src/main.js
+++ src/main.js
@@ -12,12 +12,16 @@ import {categoryCollapse} from "./js/modules/components/category-collapse";
 import {imgLazy} from "./js/modules/components/img-lazyload";
 import {toc} from "./js/modules/components/toc";
 import {initClipboard} from "./js/modules/plugins";
+// 因为 patch/index 中有内容且没有引用, 所有没有在 index 中引入
+// 完善后以下代码应改为 import { * } from './js/patch/index';
 import {activeSidebar} from "./js/patch/sidebar";
+import {addClassToTable} from './js/patch/table';
 
 basic();
 initSidebar();
 activeSidebar();
 initTopbar();
+addClassToTable();
 categoryCollapse();
 imgLazy();
 // imgPopup();
