35ddc1e70f962c17f042eaca95c5fb5dc6361d01
AirboZH <i@airbozh.cn>
2024-02-18 16:50:44 +0800

    feat: codeBlock

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import 'jquery';
 import 'bootstrap';
 import 'magnific-popup';
 import 'tocbot';
-
 import './css/style.scss';
 
 import {basic} from "./js/modules/layouts/basic";
@@ -13,15 +12,15 @@ import {imgLazy} from "./js/modules/components/img-lazyload";
 import {toc} from "./js/modules/components/toc";
 import {initClipboard} from "./js/modules/plugins";
 // 因为 patch/index 中有内容且没有引用, 所有没有在 index 中引入
-// 完善后以下代码应改为 import { * } from './js/patch/index';
-import {activeSidebar} from "./js/patch/sidebar";
-import {addClassToTable} from './js/patch/table';
+import { patch } from "./js/patch/";
 
 basic();
 initSidebar();
-activeSidebar();
 initTopbar();
-addClassToTable();
+patch.codeClass();
+patch.codeBlock();
+patch.addTableClass();
+patch.activeSidebar();
 categoryCollapse();
 imgLazy();
 // imgPopup();
