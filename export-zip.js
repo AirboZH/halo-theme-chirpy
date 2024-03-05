692d2af5461ce4f4f3dbbc2cb8b71c3442628fab
AirboZH <i@airbozh.cn>
2024-03-05 20:45:56 +0800

    pref: package dev mode

--- export-zip.js
+++ export-zip.js
@@ -2,10 +2,12 @@ const fs = require('fs');
 const archiver = require('archiver');
 
 const homedir = __dirname;
+const mode = process.env.NODE_ENV || 'dev';
 
 //const timeString = new Date().toLocaleDateString().replace(/\//g, '-'); // 日期充当hash值防止覆盖之前的压缩包
 
-const output = fs.createWriteStream(homedir + '/halo-theme-chirpy.zip');
+const outputName = mode === 'prod' ? 'halo-theme-chirpy.zip' : 'halo-theme-chirpy-dev.zip';
+const output = fs.createWriteStream(homedir + '/' + outputName);
 const archive = archiver('zip', {
   zlib: {level: 9} // 设置压缩级别
 });
@@ -17,7 +19,7 @@ archive.on('error', function (err) {
 output.on('close', function () {
   console.log(`
      --------- ---------压缩完毕--------- ---------
-        ${homedir}\\halo-theme-chirpy.zip - ${(archive.pointer() / 1024 / 1024).toFixed(1)}MB
+        ${homedir}\\${outputName} - ${(archive.pointer() / 1024 / 1024).toFixed(1)}MB
      --------- -------------------------- ---------
      `);
 });
