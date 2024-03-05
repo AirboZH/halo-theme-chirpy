9b21966aa96e7fc344d5aed68bfee09b386dab0c
AirboZH <i@airbozh.cn>
2024-03-05 20:45:56 +0800

    pref: package dev mode

---
+++ mode.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const mode = process.env.NODE_ENV || 'dev';
+
+const preHTML = `<!DOCTYPE html>
+<html xmlns:th="https://www.thymeleaf.org" lang="en" th:fragment="import()">`
+
+const postHTML = `</html>`
+
+const productionImport = `
+   <link
+      rel="stylesheet"
+      th:href="@{/assets/dist/main.css?v={version}(version=\${theme.spec.version})}"
+    />
+    <script
+      th:src="@{/assets/dist/main.js?v={version}(version=\${theme.spec.version})}"
+      type="module"
+    ></script>
+`
+
+const developmentImport = `
+   <script
+      th:src="@{http://localhost:5173/themes/halo-theme-chirpy/assets/dist/main.js}"
+      type="module"
+    ></script>
+`
+
+const outHTML = preHTML + (mode === 'prod' ? productionImport : developmentImport) + postHTML
+
+fs.writeFileSync('templates/modules/import.html', outHTML)
